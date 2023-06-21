1d92705da6eca0e279d26d6e214743aa6d5c0ded
jpmanaresi <jpmanaresi@gmail.com>
2023-06-21 13:35:40 -0300

    .

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,6 +14,10 @@
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Inria+Sans" rel="stylesheet">
     <!---->
+<!-- PWA  -->
+<meta name="theme-color" content="#6777ef"/>
+<link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+<link rel="manifest" href="{{ asset('/manifest.json') }}">
 
     <title>@yield('title')</title>
 
@@ -164,6 +168,13 @@
       
     </div>       
   </main>
-
+  <script src="{{ asset('/sw.js') }}"></script>
+  <script>
+      if (!navigator.serviceWorker.controller) {
+          navigator.serviceWorker.register("/sw.js").then(function (reg) {
+              console.log("Service worker has been registered for scope: " + reg.scope);
+          });
+      }
+  </script>
 </body>   
 </html>
